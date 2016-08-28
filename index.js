8162d8ef1296122221fe776a9483ea869a3d4e65
Adam Anderson <Aletos15@gmail.com>
2016-08-27 21:46:15 -0700

    Stopped echo.

--- index.js
+++ index.js
@@ -31,12 +31,9 @@ app.post('/webhook/', function (req, res) {
     for (let i = 0; i < messaging_events.length; i++) {
       let event = req.body.entry[0].messaging[i]
       let sender = event.sender.id
-      if (event.message && event.message.text) {
+      if (event.message && event.message.text && event.message.text === "Generic") {
         let text = event.message.text
-        if (text === 'Generic') {
-            sendGenericMessage(sender)
-        }
-        sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        sendGenericMessage(sender)
       } else if (event.postback) {
         let text = JSON.stringify(event.postback)
         sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
